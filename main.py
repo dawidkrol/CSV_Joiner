e0f56c54a06251a5187597d038c75e097be5bec5
Dawid Król <dawidkrol7@gmail.com>
2022-04-15 16:39:52 +0200

    Main method code

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import sys
+from Helpers.CSVJoiner import CSVJoiner, CSV_model, JoinType
+from Helpers.CSVReader import CSVReader
+
 if __name__ == '__main__':
-    pass
+    file_path = sys.argv[1]
+    file_path2 = sys.argv[2]
+    column_name = sys.argv[3]
+    join_type = sys.argv[4]
+
+    r = CSVReader()
+    data = CSV_model([], [])
+    data2 = CSV_model([], [])
+    r.ReadFromFile(file_path, data)
+    r.ReadFromFile(file_path2, data2)
+
+    joiner = CSVJoiner()
+    output = CSV_model([], [])
+    match join_type:
+        case 'left':
+            output = joiner.join(column_name, data, data2, JoinType.left.value)
+        case 'right':
+            output = joiner.join(column_name, data, data2, JoinType.right.value)
+        case 'inner':
+            output = joiner.join(column_name, data, data2, JoinType.inner.value)
+
+    print(output.header)
+    print(output.data)
